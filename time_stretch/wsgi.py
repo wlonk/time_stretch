88060d26ab2ec38efc033430bc7d2a7e808aa737
Ben Warren <bwarren2@gmail.com>
2014-12-11 00:09:55 -0500

    Remove whitenoise

--- time_stretch/wsgi.py
+++ time_stretch/wsgi.py
@@ -24,13 +24,13 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "time_stretch.settings.production")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = DjangoWhiteNoise(application)
 
 
 # Apply WSGI middleware here.
