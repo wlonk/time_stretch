ebf4c8ab6ce99cd254f7227bc62d309a5e46ed6f
Ben Warren <bwarren2@gmail.com>
2014-12-11 01:24:41 -0500

    Add push command

--- fabfile.py
+++ fabfile.py
@@ -7,3 +7,7 @@ def deploy():
         'python time_stretch/manage.py collectstatic' \
         ' --settings=time_stretch.settings.production --noinput'
     )
+
+def push():
+    local('git push origin master')
+    local('git push heroku master')
