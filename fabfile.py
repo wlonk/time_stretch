65d9c7dee4f3cb3bb9df1bca153986cf865a36ad
Ben Warren <bwarren2@gmail.com>
2015-01-12 17:52:05 -0500

    Make nuking s3 part of deploy.  Avoids version conflicts

--- fabfile.py
+++ fabfile.py
@@ -7,8 +7,25 @@ def push():
 
 
 def collectstatic():
+
+    # Django pipeline will try to use css on s3 if there are any there.
+    # That's bad, because we actually want our local styles to be put there.
+    # so there should never be anything there on push.
+
+    command = "python -c {0}".format(
+            """ \"""import boto
+conn = boto.connect_s3()
+bucket = conn.get_bucket('benw-personal-site')
+contents = bucket.list('css/')
+for x in contents:
+    x.delete()\"""
+    """)
+    print command
+    local(command)
+
     command = (
         'python time_stretch/manage.py collectstatic'
+        ' -i bootstrap'
         ' --settings=time_stretch.settings.production --noinput'
     )
     local(command)
